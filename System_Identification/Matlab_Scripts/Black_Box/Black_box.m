51b21c217780ac5855763ed2f4ba4e46e18a5bb3
zelja182 <zeljkoracic1996@gmail.com>
2024-09-11 08:50:09 +0200

    Adding missing model data for Black box estimated model

--- System_Identification/Matlab_Scripts/Black_Box/Black_box.m
+++ System_Identification/Matlab_Scripts/Black_Box/Black_box.m
@@ -1,5 +1,5 @@
 close all
-clear all
+% clear all
 clc
 
 test_data = "..\Ball_And_Beam\System_Identification\Matlab_Scripts\Black_Box\TestData.csv";
@@ -8,10 +8,10 @@ validatio_data = "..\Ball_And_Beam\System_Identification\Matlab_Scripts\Black_Bo
 T = readtable(test_data, 'ReadVariableNames', true);
 tst_data = iddata(T.Angles, T.PWM, 0.01);
 
-net = cascadeforwardnet([2,3,4,3,2]);
-N2 = neuralnet(net);
-sys = nlarx(tst_data, [4 4 0], N2);
-disp('Estimation is done')
+% net = cascadeforwardnet([2,3,4,3,2]);
+% N2 = neuralnet(net);
+% sys = nlarx(tst_data, [4 4 0], N2);
+% disp('Estimation is done')
 
 % Validate against test data
 figure(1);
